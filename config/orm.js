68ea3ca3bd40933ae0632d1a3356d228485867c6
anthonyknight023@gmail.com <anthonyknight023@gmail.com>
2018-03-22 18:45:49 -0400

    update orm

--- config/orm.js
+++ config/orm.js
@@ -1,24 +1,24 @@
 var connection = require('./connection.js');
 
 var orm = {
-	selectAll: function(cb){
+	selectAll: function( tablename,cb){
 
-		connection.query('SELECT * FROM burgers;', function(err, result){
+		connection.query('SELECT * FROM ??;', [tablename], function(err, result){
 			if (err) throw err;
 			// console.log(result);
 			 cb(result);
 		})
 	},
-	insertOne: function (burger_name, cb) {
+	insertOne: function (tablename, colname, burger_name, cb) {
 		
-		connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [burger_name], function (err, result) {
+		connection.query("INSERT INTO ?? (??) VALUES (?)", [ tablename ,colname, burger_name], function (err, result) {
 			if (err) throw err;			
 			 cb(result);
 		});
 	},
-	updateOne: function ( devoured, id, cb) {
+	updateOne: function ( tablename,colname, devoured, id, cb) {
 		
-		connection.query("UPDATE burgers SET devoured = ? WHERE id = ?", [ devoured, id], function (err, result) {
+		connection.query("UPDATE ?? SET ?? = ? WHERE id = ?", [tablename , colname ,devoured , id], function (err, result) {
 			if (err) throw err;			
 			cb(result);
 			
--- models/burger.js
+++ models/burger.js
@@ -3,28 +3,29 @@ var orm = require('../config/orm.js');
 var burger = {
 
   		selectAll: function(cb){
-    	orm.selectAll(function(res){
+    	orm.selectAll( 'burgers',function(res){
 
     		cb(res);
     	});     
   	},
 
   		insertOne: function(burger_name, cb){
-   		orm.insertOne(burger_name, function(res) {
+   		orm.insertOne('burgers','burger_name' , burger_name, function(res) {
    			
    			cb(res);
    			
    		});     
 
   	},
+     
   		updateOne: function( burger_id, cb){
-  		orm.updateOne(true, burger_id, function(res) {
+  		orm.updateOne('burgers','devoured',true, burger_id, function(res) {
   			
   			cb(res)
 
   		});
 	}, restoreOne: function(burger_id, cb) {
-  		orm.updateOne(false, burger_id, function(res) {
+  		orm.updateOne('burgers','devoured',false, burger_id, function(res) {
   			
   			cb(res)
 
